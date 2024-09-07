3fe4dc2ee45c6aa4ea554041ba292c0aec922771
NicoMarchiori <123659612+NicoMarchiori@users.noreply.github.com>
2024-09-07 02:46:12 -0300

    Se completa la conexion entre el backend y frontend

--- 2024_ISW_4K3/Trabajos/Resolucion Practico/Resolucion_TP6_UserStories_G2_4k3_2024/Backend/src/main/java/gurpo2/Backend/Controllers/PedidoController.java
+++ 2024_ISW_4K3/Trabajos/Resolucion Practico/Resolucion_TP6_UserStories_G2_4k3_2024/Backend/src/main/java/gurpo2/Backend/Controllers/PedidoController.java
@@ -3,7 +3,7 @@ package gurpo2.Backend.Controllers;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class PedidoController {
 
-    @GetMapping("/publicar")
+    @PostMapping("/publicar")
     public ResponseEntity<String> publicarEnvio(){
         return ResponseEntity.ok("Pedido publicado");
     }
